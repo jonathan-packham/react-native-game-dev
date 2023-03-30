f850254cc231b781252127f1aa5b15f327ceefa7
jonathan-packham <jonathan.packham@lethbridgecollege.ca>
2023-03-30 15:19:26 -0600

    Initial Commit

---
+++ components/constants.js
@@ -0,0 +1,8 @@
+import {Dimensions} from 'react';
+
+export default {
+    MAX_WIDTH: Dimensions.get('window').width,
+    MAX_HEIGHT: Dimensions.get('window').height,
+    GRID_SIZE: 15,
+    CELL_SIZE: 20,
+}
---
+++ components/styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react';
+
+const styles = StyleSheet.create({
+    canvas: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    controlBtn: {
+      backgroundColor: 'yellow',
+      width: 100,
+      height: 100,
+    },
+    controlContainer: {
+      marginTop: 10,
+    },
+    controllerRow: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+});
+
+export default styles;
